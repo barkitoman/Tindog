384b70f4fec46da72a8e089b132c4e06effa8536
Julian Barco <desarrollose2017@gmail.com>
2018-03-06 12:34:18 -0500

    Add Like And Nope Like images plus with respective alpha

--- Tindog/Controllers/HomeViewController.swift
+++ Tindog/Controllers/HomeViewController.swift
@@ -18,7 +18,9 @@ class NavigationImageView: UIImageView {
 class HomeViewController: UIViewController {
 
     @IBOutlet weak var cardView: UIView!
-    @IBOutlet weak var HomeWrapper: UIStackView!
+    @IBOutlet weak var homeWrapper: UIStackView!
+    @IBOutlet weak var likeImage: UIImageView!
+    @IBOutlet weak var nopeImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,6 +44,13 @@ class HomeViewController: UIViewController {
         var finalTransform = rotate.scaledBy(x: scale, y: scale)
         self.cardView.transform = finalTransform
         
+        if  self.cardView.center.x < (self.view.bounds.width/2 - 100){
+            self.nopeImage.alpha = min(abs(xFromCenter/100), 1)
+        }
+        if  self.cardView.center.x > (self.view.bounds.width/2 + 100){
+            self.likeImage.alpha = min(abs(xFromCenter/100), 1)
+        }
+        
         if gesture.state == .ended {
             print(self.cardView.center.x)
             if  self.cardView.center.x < (self.view.bounds.width/2 - 100){
@@ -53,7 +62,9 @@ class HomeViewController: UIViewController {
             rotate = CGAffineTransform(rotationAngle: 0)
             finalTransform = rotate.scaledBy(x: 1, y: 1)
             self.cardView.transform = finalTransform
-            self.cardView.center = CGPoint(x: self.HomeWrapper.bounds.width/2, y: self.HomeWrapper.bounds.height/2 - 50)
+            self.cardView.center = CGPoint(x: self.homeWrapper.bounds.width/2, y: self.homeWrapper.bounds.height/2 - 50)
+            self.likeImage.alpha = 0
+            self.nopeImage.alpha = 0
             
             
         }
